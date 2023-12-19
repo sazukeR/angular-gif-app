6023e0639589b19c5b1306c9fb4c1f0ba618986b
SazukeR <reinaldojcg1@gmail.com>
2023-12-19 18:31:26 -0500

    Add first project settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { GifsModule } from './gifs/gifs.module';
+import { SharedModule } from './shared/shared.module';
+
+
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +14,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    GifsModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
